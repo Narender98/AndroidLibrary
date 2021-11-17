39b219842a5c69d99027e1e2fe9e6e48a8f6f3a3
Your Name <you@example.com>
2021-11-17 13:05:24 +0530

    n

--- nkumar/src/main/java/com/nkumar/nkumar/Test.kt
+++ nkumar/src/main/java/com/nkumar/nkumar/Test.kt
@@ -4,7 +4,9 @@ import android.content.Context
 import android.widget.Toast
 
 class Test {
-    fun simpleToast(context: Context?, msg: String?) {
-        Toast.makeText(context, msg, Toast.LENGTH_SHORT).show()
+    companion object{
+        fun simpleToast(context: Context?, msg: String?) {
+            Toast.makeText(context, msg, Toast.LENGTH_SHORT).show()
+        }
     }
 }

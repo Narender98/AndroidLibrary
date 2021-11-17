f1dc38ce5b2e9cdda153be781fc16d36c0ed24ea
Your Name <you@example.com>
2021-11-17 13:22:06 +0530

    n

--- app/src/main/java/com/nkumar/androidlibrary/MainActivity.kt
+++ app/src/main/java/com/nkumar/androidlibrary/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.ui.Modifier
 import com.nkumar.androidlibrary.ui.theme.AndroidLibraryTheme
+import com.nkumar.nkumar.Test
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- nkumar/src/main/java/com/nkumar/nkumar/Test.kt
+++ nkumar/src/main/java/com/nkumar/nkumar/Test.kt
@@ -1,12 +1,22 @@
 package com.nkumar.nkumar
 
+import android.app.Activity
 import android.content.Context
 import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
 
 class Test {
     companion object{
         fun simpleToast(context: Context?, msg: String?) {
             Toast.makeText(context, msg, Toast.LENGTH_SHORT).show()
+        }
+
+        fun longSnack(context: Context, msg: String) {
+            val snackbar = Snackbar.make(
+                (context as Activity).findViewById(android.R.id.content),
+                msg, Snackbar.LENGTH_LONG
+            )
+            snackbar.show()
         }
     }
 }

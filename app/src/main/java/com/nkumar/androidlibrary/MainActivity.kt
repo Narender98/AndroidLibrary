a6de6ffc324114076a3b19fa38326808f34005a1
Your Name <you@example.com>
2021-11-17 13:00:54 +0530

    n

---
+++ app/src/main/java/com/nkumar/androidlibrary/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.nkumar.androidlibrary
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Modifier
+import com.nkumar.androidlibrary.ui.theme.AndroidLibraryTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AndroidLibraryTheme {
+                Box(modifier = Modifier.fillMaxSize()) {
+                }
+            }
+        }
+    }
+}
---
+++ nkumar/src/main/java/com/nkumar/nkumar/Test.kt
@@ -0,0 +1,10 @@
+package com.nkumar.nkumar
+
+import android.content.Context
+import android.widget.Toast
+
+class Test {
+    fun simpleToast(context: Context?, msg: String?) {
+        Toast.makeText(context, msg, Toast.LENGTH_SHORT).show()
+    }
+}
